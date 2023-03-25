78b8ff9b6a498e3345bab22f142252a5fc1d7b3b
AqsaHanif <121718004+Aqsaha@users.noreply.github.com>
2023-03-26 02:55:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+// var userName = "Aqsa";
+//  alert("Welcome " + userName + "!");
+
+//  var message = "Welcome ";
+//  var sign = "!";
+//  var greet = message+userName+sign;
+//  alert(greet);
+//  alert(message + userName + sign);
+
+//  alert("2 + 2 = " + (2+2))
+//  var city ="karachi";
+//  alert("Welcome"+userName +"!");
+//  var message = "Welcome ";
+//  var sign = "!";
+//  var greet = message+userName+sign;
+//  alert(greet);
+//  alert(message + userName + sign);
+var cityName=prompt("Write city name: ");
+// var cityName=prompt("karachi")
+alert("Welcome to the city of lights");
+var yourGender=prompt("Write your gender: ");
+// var message="Thank you";
+// var sign="!";
+// var greet=message + sign;
+// alert (greet);
+// alert(message + sign);
+// var question="How are you";
+// var defaultAnswer="Fine";
+// var spe = prompt (question, defaultAnswer);
+// var message=prompt("Good Morning Ma'am");
+// document.write=(Good Morning Ma'am)
+var message="Good Morning Ma'am";
+alert(message);
+var yourGender=prompt("Write your gender: ");
+var message="Good Morning Sir";
+alert(message);
+var trafficSignal=prompt("Enter the traffic light color: ");
+var message="Must stop";
+alert (message);
+var trafficSignal=prompt("Enter the traffic light color: ");
+var message="Ready to move";
+alert (message);
+var trafficSignal=prompt("Enter the traffic light color: ");
+var message="Move now";
+alert (message);
+var inLitres=prompt(" Enter the fuel which is remaining in your car: ");
+var message="Please refill the fuel in your car :"
+alert(message);
+var a = 4;
+if (++a === 5)
+alert("given condition for variable a is true");
+ var b = 82;
+if (b++ === 83){
+alert("given condition for variable b is true");
+}
+ var c = 12;
+if (c++ === 13){
+alert("condition 1 is true");
+}
+if (c === 13){
+alert("condition 2 is true");
+}
+if (++c < 14){
+alert("condition 3 is true");
+}
+if(c === 14){
+alert("condition 4 is true");
+}
+ var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost){
+alert("The cost equals");
+}
+if (true){
+alert("True");
+}
+if (false){
+alert("False");
+}
+var tableNumber=prompt("Enter the table number: ");
+// var tableLength=prompt("Enter the table length: ");
+var favouriteFruit=prompt("Enter your favourite fruite: ");
+// var favouriteSeet=prompt("Enter your favourite sweet: ");
+
+
+// alert("2 + 5 = " +(2+5))
